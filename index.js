88380a055862b6ea86fba560715e6b9533db8a3b
serkech@gmail.com <xcode@009>
2021-04-02 14:20:46 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv').config()
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'Keystone blog';
+const adapterConfig = { mongoUri: process.env.MONGO_URI,
+};
+
+
+/**
+ * You've got a new KeystoneJS Project! Things you might want to do next:
+ * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
+ * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
+ */
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+  cookieSecret: process.env.COOKIE_SECRET,
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true })],
+};
